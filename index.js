83868d883be78c829e24eeb94e6ff585c5dd8d21
quenbla234 <65927384+quenbla234@users.noreply.github.com>
2020-11-13 06:59:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
 const  BotName  =  'ANARGA BOT☄' ;
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const         instagram         =         'https://instagram.com/badboyz.skyz' ;
+const  telegram  =  'https://t.me/reuzzz' ;
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const    youtube    =    'https://www.youtube.com/channel/UClOruMiyMDWs1MJQnG3QFKQ' ;
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
